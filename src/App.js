72fd4f5f0d9f4124a196d0c8b4a6aaf67dee6701
Will Worth <will.worth@gmail.com>
2018-05-27 12:52:33 +0200

    font awe working as straight import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component} from "react";
 import "./App.css";
-
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> This is the vanilla app.js </h1>
+        <i className="fa fa-star"></i>
+        <i className="fa fa-star"></i>
+        <i className="fa fa-star"></i>
+        <i className="fa fa-star"></i>
+
+        
       </div>
     );
   }
